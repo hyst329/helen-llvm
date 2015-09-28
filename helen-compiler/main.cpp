68777d59fd359b5209ead59a836ef08a627e1419
hyst329 <trolleybus.1329@gmail.com>
2015-09-28 12:17:37 +0300

    added yyparse in main

--- helen-compiler/main.cpp
+++ helen-compiler/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <stdio.h>
+#include <grammar/helen.parser.hpp>
 
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
+int main(int argc, char** argv) {
+    yyin = (argc == 1) ? stdin : fopen(argv[1], "r");
+    yyparse();
     return 0;
 }
