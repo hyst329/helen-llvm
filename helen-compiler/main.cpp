1b7455db43edb496f4e9854afbff7e09dc766beb
hyst329 <trolleybus.1329@gmail.com>
2015-10-02 11:16:27 +0300

    replaced include with externs to remove missing include file errors

--- helen-compiler/main.cpp
+++ helen-compiler/main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <grammar/helen.parser.hpp>
+
+int yyparse (void);
+extern FILE *yyin;
 
 int main(int argc, char** argv) {
     yyin = (argc == 1) ? stdin : fopen(argv[1], "r");
