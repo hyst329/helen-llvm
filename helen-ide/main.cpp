fd9b6d4bb44b2f9216390f5071c0ccdf97a8df12
hyst329 <trolleybus.1329@gmail.com>
2015-11-06 12:41:04 +0300

    started adding UI

--- helen-ide/main.cpp
+++ helen-ide/main.cpp
@@ -1,4 +1,17 @@
-int main()
+#include <gtkmm.h>
+
+int main(int argc, char* argv[])
 {
-    return 0;
+    try {
+        Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.helen.ide");
+        Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("temp.glade");
+        Gtk::Window* w;
+        builder->get_widget("applicationwindow_ide", w);
+
+        return app->run(*w);
+    }
+    catch(Glib::Error e) {
+        printf("Error : %s\n", e.what().c_str());
+        return 1;
+    }
 }
