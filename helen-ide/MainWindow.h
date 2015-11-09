2f32ccd51da11c377362c4cc3fa97fcd15d7c507
hyst329 <trolleybus.1329@gmail.com>
2015-11-09 09:20:50 +0300

    added tool buttons

--- helen-ide/MainWindow.h
+++ helen-ide/MainWindow.h
@@ -3,14 +3,30 @@
 
 using namespace Gtk;
 
+enum ToolButtons
+{
+    TB_NEW,
+    TB_OPEN,
+    TB_SAVE,
+    TB_SAVE_AS,
+    TB_BUILD,
+    TB_BUILDRUN,
+    TB_RUN,
+    TB_STOP,
+    TB_QUIT,
+    // must be last entry
+    TB_COUNT
+};
+
 class MainWindow : public Window
 {
 public:
     MainWindow();
-private:
-    Box box_main = Box(ORIENTATION_VERTICAL), box_editor;
+protected:
+    Box box_main, box_editor;
     MenuBar menubar_main;
     Toolbar toolbar_main;
+    ToolButton toolbuttons[TB_COUNT];
     Statusbar statusbar_main;
     Gsv::View sourceview_editor;
     TreeView treeview_project;
