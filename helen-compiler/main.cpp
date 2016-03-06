3821f15f4cfa316581ed4c6cbc5b922ee49f7208
hyst329 <trolleybus.1329@gmail.com>
2016-03-06 18:00:02 +0300

    preparing for final release

--- helen-compiler/main.cpp
+++ helen-compiler/main.cpp
@@ -11,6 +11,7 @@
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Transforms/IPO.h>
+#include "cmake/config.h"
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
@@ -42,8 +43,12 @@ int main(int argc, char** argv)
 {
     // Boost Program Options setup
     po::options_description desc("Compiler options");
-    desc.add_options()("help,h", "display this help")("version,v", "display version")("input-file", po::value<string>(),
-        "input file")("include-path,I", po::value<vector<string> >(), "path to include files");
+    desc.add_options()
+        ("help,h", "display this help")
+        ("version,v", "display version")
+        ("dump,D", "dump LL output to stdout (for debug)")
+        ("input-file", po::value<string>(), "input file")
+        ("include-path,I", po::value<vector<string> >(), "path to include files");
     po::positional_options_description p;
     p.add("input-file", -1);
     po::variables_map vm;
@@ -51,7 +56,11 @@ int main(int argc, char** argv)
     po::notify(vm);
     if(vm.count("help") || vm.empty()) {
         cout << desc << "\n";
-        return 1;
+        return 0;
+    }
+    if(vm.count("version")) {
+        cout << PROJECT_VERSIONED_NAME << "\n";
+        return 0;
     }
     if(vm.count("include-path"))
         includePaths = vm["include-path"].as<vector<string> >();
@@ -86,7 +95,10 @@ int main(int argc, char** argv)
     }
     AST::builder.CreateRet(ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 0));
     pm.run(*(AST::module.get()));
-    AST::module->dump();
+    if(vm.count("dump")) {
+        AST::module->dump();
+        fflush(stdout);
+    }
     // FIXME: Replace with positional options
     string filename = vm["input-file"].as<string>() + ".bc";
     std::error_code ec;
