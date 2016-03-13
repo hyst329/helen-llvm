853eab3070200a1e1233b0c00cdf8d9d9a321a9a
hyst329 <trolleybus.1329@gmail.com>
2016-03-13 21:17:20 +0300

    helmake boilerplate

--- helmake/main.cpp
+++ helmake/main.cpp
@@ -1,4 +1,25 @@
+#include <boost/any.hpp>
+#include <map>
+#include <string>
+#include <vector>
+#include <process.h>
+
+using namespace std;
+
+map<string, vector<boost::any> > variableMap;
+
+int yyparse();
+extern FILE* yyin;
+
 int main()
 {
+    yyin = fopen("helmkf", "r");
+    if(!yyin) {
+        fprintf(stderr, "*** No helmkf file in the current dir. Stop. ***\n");
+        return 1;
+    }
+    // Setting default values
+    variableMap["helen_compiler"] = vector<boost::any>(1, boost::any(string("helen_compiler")));
+    yyparse();
     return 0;
 }
