37e030a1809c2d3a7ebf6d6519aac2735abe1c91
hyst329 <trolleybus.1329@gmail.com>
2016-03-16 18:31:37 +0300

    prepared to v0.1.1

--- helmake/main.cpp
+++ helmake/main.cpp
@@ -1,10 +1,12 @@
 #include <boost/any.hpp>
+#include <boost/filesystem.hpp>
 #include <map>
 #include <string>
 #include <vector>
 #include <process.h>
 
 using namespace std;
+namespace fs = boost::filesystem;
 
 map<string, vector<boost::any> > variableMap;
 
@@ -19,7 +21,37 @@ int main()
         return 1;
     }
     // Setting default values
-    variableMap["helen_compiler"] = vector<boost::any>(1, boost::any(string("helen_compiler")));
+    variableMap["HELEN_COMPILER"] = vector<boost::any>(1, boost::any(string("helc")));
+    variableMap["PROJECT"] = vector<boost::any>(1, boost::any(string("untitled")));
+    variableMap["FLAGS"] = vector<boost::any>(1, boost::any(string("")));
     yyparse();
+    auto sources = variableMap["SOURCES"];
+    if(sources.empty()) {
+        fprintf(stderr, "*** Provided helmkf file contains no SOURCES variable. Stop. ***\n");
+        return 1;
+    }
+    fs::path p = "./build";
+    fs::create_directories(p);
+    FILE* makefile = fopen((p / "Makefile").generic_string().c_str(), "w");
+    string helc = boost::any_cast<string>(variableMap["HELEN_COMPILER"][0]);
+    fprintf(makefile, "HELC = %s\n", helc.c_str());
+    fprintf(makefile, "LLC = llc\n", helc.c_str());
+    fprintf(makefile, "LD = gcc\n");
+    string project = boost::any_cast<string>(variableMap["PROJECT"][0]);
+    string flags = boost::any_cast<string>(variableMap["FLAGS"][0]);
+    fprintf(makefile, "FLAGS = %s\n", flags.c_str());
+    fprintf(makefile, "all: %s\n\n%s: ", project.c_str(), project.c_str());
+    for(boost::any& s : sources) {
+        fs::path p = boost::any_cast<string>(s);
+        string abs = fs::canonical(p).generic_string();
+        fprintf(makefile, "%s.bc ", abs.c_str());
+    }
+    fprintf(makefile, "\n\t$(LD) ");
+    for(boost::any& s : sources) {
+        fs::path p = boost::any_cast<string>(s);
+        string abs = fs::canonical(p).generic_string();
+        fprintf(makefile, "%s.bc ", abs.c_str());
+    }
+    fclose(makefile);
     return 0;
 }
