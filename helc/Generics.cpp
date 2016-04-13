d23687c2305f88d8dc873599937ad94d8c4fa346
hyst329 <trolleybus.1329@gmail.com>
2016-04-13 17:30:12 +0300

    added concept for generic function instantiation

--- helc/Generics.cpp
+++ helc/Generics.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 namespace Helen
 {
-    void GenericFunction::instantiate(vector<Type*> typeParams)
+    void GenericFunction::instantiate(map<string, Type*> typeParams)
     {
-        
+        // for (auto p : typeParams) add type from p to AST::types
+        // theAST->codegen();
+        // for (auto p : typeParams) remove type from p from AST::types
     }
 }
