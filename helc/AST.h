9cbda57fc27addc7715d3f0e44fc718b580ff213
hyst329 <trolleybus.1329@gmail.com>
2019-01-06 20:58:15 +0300

    removed llvm dependency, added libfirm instead

--- helc/AST.h
+++ helc/AST.h
@@ -5,21 +5,12 @@
 #ifndef PROJECT_AST_H
 #define PROJECT_AST_H
 
-#include <llvm/Analysis/Passes.h>
-#include <llvm/IR/Function.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Value.h>
-#include <llvm/IR/Verifier.h>
-#include <llvm/Transforms/Scalar.h>
 #include <map>
 #include <memory>
 #include <set>
 #include <stack>
 #include <vector>
 
-using namespace llvm;
 using namespace std;
 namespace Helen {
 class GenericFunction;
